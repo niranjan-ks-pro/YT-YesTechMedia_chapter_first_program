0eb5e42ef0d831545b2c1b25a8d2e4299b2dcc97
Niranjan-KS <131178323+niranjan-ks-pro@users.noreply.github.com>
2023-06-22 18:34:49 +0530

    this is bin folder

---
+++ test/chapter_first_program_test.dart
@@ -0,0 +1,8 @@
+import 'package:chapter_first_program/chapter_first_program.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
